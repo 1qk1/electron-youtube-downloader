63d532450c81b7794fde4b5a6951837449d95589
Panagiotis <1qk231@gmail.com>
2019-01-05 18:19:00 +0200

    major improvements

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,28 +1,49 @@
 const { ipcRenderer } = require("electron");
 const { dialog } = require("electron").remote;
-let outputFolder;
+const $ = require("jquery");
+const Store = require("electron-store");
+const store = new Store();
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems);
+let outputFolder = store.get("downloadFolder") || "";
+
+document.addEventListener("DOMContentLoaded", function() {
+  const elems = document.querySelectorAll("select");
+  M.FormSelect.init(elems);
 });
 
+const initValues = () => {
+  document.querySelector(".output-folder-preview").textContent = outputFolder;
+  const downloadQuality = store.get("downloadQuality") || "";
+  $('[name="downloadQuality"').val(downloadQuality);
+};
 
-document.querySelector("form").addEventListener("submit", e => {
+$("form").on("submit", function(e) {
   e.preventDefault();
   if (outputFolder === undefined) return;
   const downloadInfo = {};
-  const inputs = document.querySelectorAll('[name]');
-  inputs.forEach(inp => downloadInfo[inp.name] = inp.value);
+  const inputs = document.querySelectorAll("[name]");
+  inputs.forEach(inp => (downloadInfo[inp.name] = inp.value));
   ipcRenderer.send("page:download", {
     ...downloadInfo,
     outputFolder
   });
 });
 
-document.querySelector(".select-folder").addEventListener("click", e => {
-  outputFolder = dialog.showOpenDialog({
+$('[name="downloadQuality"').on("change", e => {
+  const selectElement = e.target;
+  const selectedQuality =
+    selectElement.options[selectElement.selectedIndex].value;
+  store.set("downloadQuality", selectedQuality);
+  downloadQuality = selectedQuality;
+});
+
+$(".select-folder").on("click", e => {
+  const selectedFolder = dialog.showOpenDialog({
     properties: ["openDirectory"]
   })[0];
+  outputFolder = selectedFolder;
+  store.set("downloadFolder", selectedFolder);
   document.querySelector(".output-folder-preview").textContent = outputFolder;
 });
+
+initValues();
