c4c9f464e071adf80411452044ddba8f81c2945e
Panagiotis <1qk231@gmail.com>
2019-01-05 18:18:15 +0200

    fixed a lot of stuff and made improvements

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 const fs = require("fs");
 const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
-const randomString = require("randomstring");
 const ytlist = require("youtube-playlist");
 const sanitizer = require("sanitize-filename");
 
@@ -15,19 +14,25 @@ const initWindow = () => {
     width: 1200,
     height: 760
   });
+
+  mainWindow.on("close", () => {
+    clearDownloads();
+  });
+
   mainWindow.loadFile(`${__dirname}/pages/main/main.html`);
 };
 
-const loadMain = () => {
+const clearDownloads = () => {
   if (ffmpegProccess.length !== 0) {
     for (let proccess in ffmpegProccess) {
-      ffmpegProccess[proccess].task.kill('SIGSTOP');
-      fs.unlink(`${downloadFolder}/${proccess}.mp3`, () => {
-        console.log('deleted file', proccess);
-      })
+      ffmpegProccess[proccess].task.kill();
     }
     ffmpegProccess = {};
   }
+};
+
+const loadMain = () => {
+  clearDownloads();
   mainWindow.loadFile(`${__dirname}/pages/main/main.html`);
 };
 
@@ -37,35 +42,34 @@ const loadDownload = (e, downloadInfo) => {
 };
 
 const download = downloadInfo => {
-  const { url, urlType, outputFolder, downloadQuality } = downloadInfo;
-  downloadFolder = outputFolder
-  if (urlType === "video") {
-    downloadOne({url, downloadQuality});
-  } else if (urlType === "playlist") {
-    downloadPlaylist({url, downloadQuality});
+  const { url, outputFolder, downloadQuality } = downloadInfo;
+  downloadFolder = outputFolder;
+  if (ytdl.validateID(url) || ytdl.validateURL(url)) {
+    downloadOne({ url, downloadQuality });
+  } else {
+    downloadPlaylist({ url, downloadQuality });
   }
 };
 
 const downloadOne = async ({ url, downloadQuality }) => {
+  // if url is invalid return
   if (!ytdl.validateURL(url) && !ytdl.validateID(url)) return;
+  // get basic video info
   const videoInfo = await ytdl.getBasicInfo(url);
-  const { title, video_id } = videoInfo;
-  videoInfo.title = sanitizer(title);
-  // Write video data in a .json to read them later and
-  // replace the 320kbps option with "highest"
-  // fs.writeFileSync(`${__dirname}/${videoInfo.video_id}.json`, JSON.stringify(videoInfo));
+  let { title, video_id, thumbnail_url } = videoInfo;
+  title = sanitizer(title);
+  // add "vid" at the start of the video id
+  video_id = `vid-${video_id}`;
 
-  (() => {
-    const { thumbnail_url } = videoInfo
-    mainWindow.webContents.send("info", {
-      title,
-      thumbnail_url,
-      video_id
-    });
-  })()
-
+  // send the info back to the main window
+  mainWindow.webContents.send("info", {
+    title,
+    thumbnail_url,
+    video_id
+  });
+  // start the download
   const stream = ytdl(url, {
-    // quality: "highestaudio"
+    // quality: "highestaudio",
     filter: "audio",
     highWaterMark: 1024 * 1024 * 10
   })
@@ -74,29 +78,53 @@ const downloadOne = async ({ url, downloadQuality }) => {
     })
     .on("progress", (chunk, downloaded, total) => {
       const progress = (downloaded / total) * 100;
-      mainWindow.webContents.send("progress", {progress, video_id});
+      mainWindow.webContents.send("progress", { progress, video_id });
     });
+
+  // encode the stream to mp3
   const task = ffmpeg(stream)
     .audioBitrate(Number(downloadQuality))
+    .audioCodec("libmp3lame")
     .save(`${downloadFolder}/${video_id}.mp3`)
+    .on("error", () => {
+      stream.pause();
+      stream.destroy();
+      fs.unlink(`${downloadFolder}/${video_id}.mp3`, () => {
+        console.log("deleted file");
+      });
+    })
     .on("end", () => {
+      // const filenameRegex = /(["/\\:*?<>|])/gi;
       fs.rename(
         `${downloadFolder}/${video_id}.mp3`,
-        `${downloadFolder}/${title}.mp3`,
+        // `${downloadFolder}/${title.replace(filenameRegex, "")}.mp3`,
+        `${downloadFolder}/${sanitizer(title)}.mp3`,
         () => {
+          mainWindow.webContents.send("progress", {
+            completed: true,
+            video_id,
+            progress: 100
+          });
           console.log("finished downloading");
-          // ffmpegProccess[video_id].task.kill("SIGSTOP");
           delete ffmpegProccess[video_id];
         }
       );
     });
-  ffmpegProccess[video_id] = ({ video_id, task });
+  ffmpegProccess[video_id] = { video_id, task };
 };
 
-const downloadPlaylist = async info => {
-  const {url, downloadQuality} = info
-  const list = await ytlist(url, "id");
-  list.data.playlist.forEach(id => downloadOne({ url: id, downloadQuality }));
+const downloadPlaylist = info => {
+  const { url, downloadQuality } = info;
+  ytlist(url, "id")
+    .then(list => {
+      console.log(list.data.playlist, "list");
+      list.data.playlist.forEach(id =>
+        downloadOne({ url: id, downloadQuality })
+      );
+    })
+    .catch(error => {
+      console.log("invalid url");
+    });
 };
 
 app.on("ready", initWindow);
