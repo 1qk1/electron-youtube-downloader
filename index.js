f6e6dbf60c5a9799c45e919f3461b85425444b7d
Panagiotis <1qk231@gmail.com>
2019-04-23 20:48:38 +0300

    move download function to new file

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require("electron"),
-  fs = require("fs"),
   ytdl = require("ytdl-core"),
   ytlist = require("youtube-playlist"),
-  sanitizer = require("sanitize-filename"),
   upath = require("upath"),
-  convert = require("./convert"),
-  state = require("./state");
+  state = require("./state"),
+  downloadOne = require("./download");
 
 let mainWindow;
-let downloadFolder;
 
 const initWindow = () => {
   mainWindow = new BrowserWindow({
@@ -41,71 +38,12 @@ const loadDownload = (e, downloadInfo) => {
 
 const download = downloadInfo => {
   const { url, outputFolder, downloadQuality } = downloadInfo;
-  downloadFolder = upath.normalize(outputFolder);
+  process.env.DOWNLOAD_FOLDER = upath.normalize(outputFolder);
   if (ytdl.validateID(url) || ytdl.validateURL(url)) {
-    downloadOne({ url, downloadQuality });
+    downloadOne({ url, downloadQuality, mainWindow });
   } else {
     downloadPlaylist({ url, downloadQuality });
   }
-};
-
-const downloadOne = async ({ url, downloadQuality }) => {
-  // if url is invalid return
-  if (!ytdl.validateURL(url) && !ytdl.validateID(url)) return;
-  // get basic video info
-  const videoInfo = await ytdl.getBasicInfo(url);
-  let { title, video_id, thumbnail_url } = videoInfo;
-  title = sanitizer(title);
-  // add "vid" at the start of the video id
-  video_id = `vid-${video_id}`;
-
-  // send the info back to the main window
-  mainWindow.webContents.send("info", {
-    title,
-    thumbnail_url,
-    video_id
-  });
-
-  // start the download
-  const readable = ytdl(url, {
-    filter: "audio",
-    highWaterMark: 1024 * 1024 * 10
-  })
-    .on("error", error => {
-      errorHandler(error, video_id);
-    })
-    .on("progress", (chunk, downloaded, total) => {
-      const progress = (downloaded / total) * 100;
-      mainWindow.webContents.send("progress", { progress, video_id });
-      // console.log(progress);
-    })
-    // after download ends convert the file to mp3
-    .on("end", () => {
-      convert(
-        video_id,
-        title,
-        downloadFolder,
-        downloadQuality,
-        thumbnail_url,
-        error => {
-          mainWindow.webContents.send("progress", {
-            completed: true,
-            video_id,
-            progress: 100
-          });
-          if (error !== null) {
-            return errorHandler(error, video_id);
-          }
-          state.deleteProcess(video_id);
-        }
-      );
-    });
-  // add the readable stream in the processes object so we can control it
-  state.addProcess(video_id, "readable", readable);
-  let writeable = readable.pipe(
-    fs.createWriteStream(`${downloadFolder}/${video_id}.mp3`)
-  );
-  state.addProcess(video_id, "writeable", writeable);
 };
 
 const downloadPlaylist = info => {
